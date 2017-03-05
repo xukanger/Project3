4be7efd917c522f06ab33e51fbde7189e029e2e7
llf <1172593703@qq.com>
2017-03-05 13:07:25 +0800

    公共类

---
+++ src/main/java/just/yt/service/ExamineeService.java
@@ -0,0 +1,64 @@
+package just.yt.service;
+
+import just.yt.dao.ExamineeMapper;
+import just.yt.model.Examinee;
+import just.yt.model.ExamineeExample;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by llf on 2017/3/5.
+ *考生信息类的增删改查
+ */
+@Service
+public class ExamineeService {
+
+
+    @Resource
+    ExamineeMapper examineeMapper;
+
+
+    public Examinee insert(Examinee examinee){
+        examineeMapper.insertSelective(examinee);
+        return examinee;
+    }
+
+    public List<Examinee> getAll(){
+        return examineeMapper.selectByExample(new ExamineeExample());
+    }
+
+    public Examinee update(Examinee examinee){
+        ExamineeExample examineeExample=new ExamineeExample();
+        examineeExample.createCriteria().andIdEqualTo(examinee.getId());
+        examineeMapper.updateByExampleSelective(examinee,examineeExample);
+        return examinee;
+    }
+
+    public boolean delete(Long id){
+        return examineeMapper.deleteByPrimaryKey(id) > 0;
+    }
+
+    public void save(Examinee examinee){
+        examineeMapper.insert(examinee);
+    }
+
+    public Integer count(){
+        return examineeMapper.countByExample(new ExamineeExample());
+    }
+
+    public List<Examinee> getByPage(int pageSize,int pageNo){
+        ExamineeExample examineeExample=new ExamineeExample();
+        examineeExample.setLimitStart((pageNo - 1) * pageSize);
+        examineeExample.setLimitSize(pageSize);
+        return examineeMapper.selectByExample(examineeExample);
+    }
+
+    public Examinee getById(Long id){
+        return examineeMapper.selectByPrimaryKey(id);
+    }
+
+
+
+}
---
+++ src/main/java/just/yt/service/TestMarkService.java
@@ -0,0 +1,57 @@
+package just.yt.service;
+
+import just.yt.dao.TestMarkMapper;
+import just.yt.model.TestMark;
+import just.yt.model.TestMarkExample;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@Service("testMarkService")
+public class TestMarkService {
+
+    @Resource
+    TestMarkMapper testMarkMapper;
+
+
+    public TestMark insert(TestMark testMark){
+        testMarkMapper.insertSelective(testMark);
+        return testMark;
+    }
+
+    public List<TestMark> getAll(){
+        return testMarkMapper.selectByExample(new TestMarkExample());
+    }
+
+    public TestMark update(TestMark testMark){
+        TestMarkExample TestMarkExample=new TestMarkExample();
+        TestMarkExample.createCriteria().andIdEqualTo(testMark.getId());
+        testMarkMapper.updateByExampleSelective(testMark,TestMarkExample);
+        return testMark;
+    }
+
+    public boolean delete(Long id){
+        return testMarkMapper.deleteByPrimaryKey(id) > 0;
+    }
+
+    public void save(TestMark testMark){
+        testMarkMapper.insert(testMark);
+    }
+
+    public Integer count(){
+        return testMarkMapper.countByExample(new TestMarkExample());
+    }
+
+    public List<TestMark> getByPage(int pageSize,int pageNo){
+        TestMarkExample TestMarkExample=new TestMarkExample();
+        TestMarkExample.setLimitStart((pageNo - 1) * pageSize);
+        TestMarkExample.setLimitSize(pageSize);
+        return testMarkMapper.selectByExample(TestMarkExample);
+    }
+
+    public TestMark getById(Long id){
+        return testMarkMapper.selectByPrimaryKey(id);
+    }
+}
